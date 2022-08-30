d082e8c6976121478958722ffc2e1849937fd0e2
Nasta <109822733+nasta11@users.noreply.github.com>
2022-08-30 18:08:51 +0300

    Ex0011

---
+++ Ex0011/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+class HelloWorld {
+  static bool isPal(int n)
+  {
+      int r=0,nn=n;
+      while (n>0)
+      {
+          r=r*10+n%10;
+          n=n/10;
+      }
+      return r==nn;
+  }
+  static void Main() {
+    int n;  
+    Console.Write("n=");
+    n=Convert.ToInt32(Console.ReadLine());
+    if (isPal(n))
+       Console.WriteLine("Палиндром");
+    else   
+       Console.WriteLine("Не палиндром");
+  }
+}
+
+
+
+
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2){
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
+
+
+
+
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
